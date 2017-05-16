44cd98e14f8437d968492a5e8c2b67a19ac5aea9
11arco <marco.bellmann95@web.de>
2017-05-16 19:58:18 +0200

    Blatt4 Intervall [-1000,1000]

--- src/Blatt4/Application.java
+++ src/Blatt4/Application.java
@@ -10,36 +10,38 @@ public class Application {
 		if (args.length == 0) {
 
 			points = new Point[(int) (Math.random() * 10)];
-		}
-		if (args.length != 6) {
-			System.out.println("Falsche Eingabe.");
-			System.out.println("Bitte 6 Parameter eingeben.");
-
-			return;
-		} else {
-			try {
-				int i = Integer.parseInt(args[0]);
-				points = new Point[3];
-
-				values = new double[(int) (2)];
-				for (int i1 = 0; i1 < points.length; i1++) {
-					for (int j = i1; j < values.length; j=j+2) {
-						values[j] = Double.parseDouble(args[j]);
-					}
-					points[i1] = new Point(values);
+			values = new double[(int) (Math.random() * 6)];
+			for (int i = 0; i < points.length; i++) {
+				for (int j = 0; j < values.length; j++) {
+					values[j] = (Math.random() * 2000)-1000;
 				}
-			} catch (IllegalArgumentException e) {
-				System.out.println(e);
-				return;
-			}
-		}
+				points[i] = new Point(values);
+				
 
-		values = new double[(int) (Math.random() * 6)];
-		for (int i = 0; i < points.length; i++) {
-			for (int j = 0; j < values.length; j++) {
-				values[j] = (Math.random() * 10);
 			}
-			points[i] = new Point(values);
+		} else {
+			if (args.length != 6) {
+				System.out.println("Falsche Eingabe.");
+				System.out.println("Bitte 6 Parameter eingeben.");
+
+				return;
+			} else {
+				try {
+					int i = Integer.parseInt(args[0]);
+					points = new Point[3];
+
+					values = new double[(int) (2)];
+					for (int i1 = 0; i1 < points.length; i1++) {
+						for (int j = i1; j < values.length; j = j + 2) {
+							values[j] = Double.parseDouble(args[j]);
+						}
+						points[i1] = new Point(values);
+					}
+				} catch (IllegalArgumentException e) {
+					System.out.println(e);
+					return;
+				}
+			}
 
 		}
 		Simplex s = new Triangle(points.length, points);
